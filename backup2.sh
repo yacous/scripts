ddfb555801a81b9096b9039ac52b4c68c60ca3a8
yacous <80653436+yacous@users.noreply.github.com>
2021-08-26 22:54:04 +0100

    Create backup2.sh

---
+++ backup2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# mon premier vrai script pour créer un backup
+# veuillez indiquer les dossiers à sauvegarder sous forme de : $ sh script.sh /tmp/folder1 /tmp/file2 /etc
+
+# variables
+folders_to_backup=$@
+backup_destination="/tmp/backup"
+
+# declarer les fonctions
+create_archive_filename(){
+        day=$(date +%A)
+        hostname=$(hostname -s)
+        archive_file="$hostname-$day.tgz"
+}
+
+print_start_status_message(){
+        date
+        echo "Backing up $folders_to_backup to $backup_destination/$archive_file"
+        }
+
+backup_files(){
+        tar czfP $backup_destination/$archive_file $folders_to_backup --absolute-names
+}
+
+print_end_status_message(){
+        echo "Backup finished"
+        date
+}
+
+estimate_file_space_usage(){
+        du -sh $backup_destination/$archive_file
+}
+
+main(){
+        create_archive_filename
+        print_start_status_message
+        backup_files
+        print_end_status_message
+        estimate_file_space_usage
+}
+
+# lancer la fonction main qui englobe toutes les fonctions
+main
