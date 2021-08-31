36ea873e7b281311a06c0108b384d4528ce8fd07
yacous <yacous2005@gmail.com>
2021-08-31 20:17:54 +0100

    deploy1

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,26 @@ echo "
 Options:
 	--create : to create file (touch)
 	--delete : to delete file/folder (rm -rf)
+
 "
+option=$1
+file=$2
+
+create_file(){
+	touch $file
+        echo "$file has been created"
+}
+
+delete_file(){
+        rm -rf $file
+        echo "$file has been deleted"
+}
+
+
 if [ "$1" = "--create" ]; then
-	touch $2
-	echo $2 has been created
+	create_file
 elif [ "$1" = "--delete" ]; then
-	rm -rf $2
-	echo $2 has been deleted
+	delete_file
+else
+	echo "Please use an option"
 fi
