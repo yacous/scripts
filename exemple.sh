29493c5907553daed6af8eaf59828c1bb1d6cfbe
yacous <yacous2005@gmail.com>
2021-09-01 00:01:46 +0100

    example

---
+++ exemple.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+########################################################
+#						       #
+# Description : Description du script		       #
+#						       #
+# Auteur : Yacous				       #
+#						       #
+# Date : 01/01/2021				       #
+#						       #
+########################################################
+
+echo "
+
+Options :
+
+	--create : create file
+
+	--delete : delete file / folder
+
+	--drop : drop file
+
+	--start : start service
+
+	--infos : help
+
+
+"
+
