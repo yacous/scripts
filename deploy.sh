819198c2ce2e9513a852aed4f63ae934c264596a
yacous <yacous2005@gmail.com>
2021-09-13 10:46:50 +0100

    Add details function

--- deploy.sh
+++ deploy.sh
@@ -17,12 +17,22 @@ delete_file(){
         echo "$file has been deleted"
 }
 
+details (){
+	if [[ -a $file ]] ; then      
+		echo $file
+        	echo "Sa taille est de $(du -sh $file 2> /dev/null | awk -F "/" '{print $1}')"
+        	echo "Son type est $(file $file | awk -F ":" '{print $2}')"
+	else 
+		echo "The File / Folder is not found"
+	fi
+}
 show_options(){
         echo "
                 Options:
-                        --create : to create file (touch)
-                        --delete : to delete file/folder (rm -rf)
-			--help : show help
+                        --create  : to create file (touch)
+                        --delete  : to delete file/folder (rm -rf)
+			--details : to print file/folder details
+			--help    : show help
 
         "
 
@@ -33,6 +43,8 @@ if [ "$1" = "--create" ]; then
 	create_file
 elif [ "$1" = "--delete" ]; then
 	delete_file
+elif [ "$1" = "--details" ]; then
+	details
 elif [ "$1" = "--help" ]; then
 	show_options
 else
