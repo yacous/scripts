e62f322c9ecc379d39297db00608474b9c287453
yacous <80653436+yacous@users.noreply.github.com>
2022-05-13 11:28:44 +0200

    functions add

--- go_pass.sh
+++ go_pass.sh
@@ -4,7 +4,7 @@
 #                                                                              #
 # Description : Script pour tester un mot de passe sur plusieurs IP en ssh     #
 #                                                                              #
-# Auteur : Yacous                                                              #
+# Auteur :                                                                     #
 #                                                                              #
 # Date : 11/05/2022                                                            #
 #                                                                              #
@@ -14,40 +14,56 @@
 #                                                                              #
 ################################################################################
 
-# help en cas de besoin
-
-if [[ $1 = "-help" ]]
-then
-    head -n 16 go_pass.sh | tail -n 15
-    exit 0
-fi
-
-
 # décrlarer les variables :
 
 ip=($(cat ip.txt))
 user="root"
 
 
-# demander le passwd
+# help en cas de besoin :
+
+help_function(){
+
+    if [[ $1 = "-help" ]]
+    then
+         head -n 16 go_pass.sh | tail -n 15
+         exit 0
+    fi
+}
+
+
+# demander le passwd à tester :
+
+passwd_ask(){
 
     echo -n 'Please enter the password : '
     read -s passwd
     printf "\n"
+}
 
 
 # boucle for pour le array de la variable $ip en totalité (@) puis sshpass user @ ip ($i), ensuite if host accessible stdout renvoyé vers le fichier hosts.txt, sinon msg 'pas accessible'
-# stdout des cnx ssh est renvoyé vers /dev/null
+# stdout des cnx ssh est renvoyé vers /dev/null :
 
-for i in ${ip[@]}
-do
-    sshpass -p $passwd ssh -o ConnectTimeout=1 -o StrictHostKeyChecking=no -o GSSAPIAuthentication=no  $user@$i "printf $i" 2> /dev/null
+sshpass_launch(){
 
-    if [ $? = "0" ]
-    then
-        echo $i >> hosts.txt
-        echo " a été ajoutée au fichier ./hosts.txt"
-    else
-        echo "$i n'est pas accessible"
-    fi
-done
+    for i in ${ip[@]}
+    do
+        sshpass -p $passwd ssh -o ConnectTimeout=1 -o StrictHostKeyChecking=no -o GSSAPIAuthentication=no  $user@$i "printf $i" 2> /dev/null
+
+        if [ $? = "0" ]
+        then
+            echo $i >> hosts.txt
+            echo " a été ajoutée au fichier ./hosts.txt"
+        else
+            echo "$i n'est pas accessible"
+        fi
+    done
+}
+
+
+#lancer les fonctions (en cas de besoin, définir une fonction 'main' qui englobe toutes les fonctions et la lancer en une seule fois) :
+
+    help_function
+    passwd_ask
+    sshpass_launch
