fde2a7fc5416a5cf5d4f085f49e65dd6cfb6cacb
yacous <80653436+yacous@users.noreply.github.com>
2022-05-13 16:53:32 +0200

    Update example2.sh

--- example2.sh
+++ example2.sh
@@ -6,7 +6,7 @@
 #                                                                              #
 # Auteur : Yacous                                                              #
 #                                                                              #
-# Date : 11/05/2022                                                            #
+# Date : 12/05/2022                                                            #
 #                                                                              #
 # Utilisation : veuillez renseigner vos adresses IP dans un fichier ./ip.txt,  #
 # puis lancez le script 'sh go_pass.sh, les adresses IP dont le mot de passe   #
@@ -16,13 +16,8 @@
 
 # help en cas de besoin
 
-help_function(){
-
     if [[ $1 = "-help" ]]
     then
          head -n 16 go_pass.sh | tail -n 15
          exit 0
     fi
-}
-
-help_function
