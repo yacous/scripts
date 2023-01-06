25ca7310f3e238ae08077c3c1d779407c8a66f7c
yacous <80653436+yacous@users.noreply.github.com>
2023-01-06 17:10:09 +0100

    Update and rename sedip.sh to ssh_rm.sh

--- ssh_rm.sh
+++ ssh_rm.sh
@@ -7,7 +7,7 @@
 #                                                                              #
 # Auteur : Yacous                                                              #
 #                                                                              #
-# Date : 04/11/2022                                                            #
+# Création : 04/11/2022         Update : 06/01/2023                            #
 #                                                                              #
 # Utilisation : veuillez renseigner vos adresses IP dans un fichier ./ip.txt,  #
 # puis lancez le script 'sh go_pass.sh, les adresses IP dont le mot de passe   #
@@ -19,12 +19,52 @@
 
     if [[ $1 = "-help" ]]
     then
-         head -n 17 sedip.sh | tail -n 15
+         head -n 17 ssh_rm.sh | tail -n 15
          exit 0
     fi
 
 # supprimer la ligne où il y a l'IP $1 du fichier /root/.ssh/known_hosts
 
-sed -i /$1/d /root/.ssh/known_hosts
+ip=$1
+path="/root/.ssh/known_hosts"
 
-echo L\'adresse IP $1 n\'y est plus !
+grep $1 $path
+
+if [[ $? = "1" ]]
+then
+  echo "L'adresse IP $1 n'est pas présente dans le fichier $path"
+  exit 0
+else
+  echo -n "Do you want to delete $ip from $path ? (y/n)"
+  read delete
+
+  if [[ $delete = "y" ]]
+  then
+    sed -i /$ip/d $path
+    echo "L'adresse IP $ip vient d'être supprimée "
+    echo -n "Do you want to connect to $ip ? (y/n)"
+    read connect
+
+    if [[ $connect = "y" ]]
+    then
+      ssh root@$ip
+    elif [[ $connect = "n" ]]
+    then
+      echo "exit"
+      exit 0
+    else
+      echo "choix non reconnu ! "
+      exit 0
+    fi
+
+  elif [[ $delete = "n" ]]
+  then
+    echo "aucune entrée n'a été supprimé"
+    echo "exit"
+    exit 0
+  else
+    echo "choix non reconnu ! "
+    echo "exit"
+    exit 0
+  fi
+fi
